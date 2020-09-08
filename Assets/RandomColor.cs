7b9964c025a8ed0fb302d6f07808d8d5d3c3d476
MATsogas <mrst5gs@yahoo.gr>
2020-09-08 18:17:39 +0300

    Make Tiles Random Color on Spawn

---
+++ Assets/RandomColor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomColor : MonoBehaviour
+{
+    public GameObject cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube.GetComponent<Renderer>().material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+    }
+}
