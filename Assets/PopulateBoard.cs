8df079e9edbc5db13da933464ab30d412ee81f72
MATsogas <mrst5gs@yahoo.gr>
2020-09-24 20:12:33 +0300

    Create Player Spawning on Board Center

--- Assets/PopulateBoard.cs
+++ Assets/PopulateBoard.cs
@@ -5,29 +5,33 @@ using UnityEngine;
 public class PopulateBoard : MonoBehaviour
 {
 
-    public GameObject boardRef;
-    public GameObject tileRowRef;
-    public GameObject tileRef;
-
-    public GameObject TileRowRef { get => tileRowRef; set => tileRowRef = value; }
+    public GameObject board;
+    public GameObject tileRow;
+    public GameObject tile;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
         // Get Board's position for later use
-        float boardRefX = boardRef.transform.position.x;
-        float boardRefY = boardRef.transform.position.y;
+        float boardPosX = board.transform.position.x;
+        float boardPosY = board.transform.position.y;
 
-        //Create 7x5 Board
+        // Get Rows' scale on X axis for later use
+        float rowScaleX = tileRow.transform.localScale.x;
+
+        // Create 7x5 Board
         for (int i = 0; i < 7; i++)
         {
-            GameObject currentRow = Instantiate(tileRowRef, new Vector3(0, boardRefY + i * -0.95f, 9.5f), Quaternion.identity, boardRef.transform);
+            GameObject currentRow = Instantiate(tileRow, new Vector3(0, boardPosY + i * -0.95f, 9.5f), Quaternion.identity, board.transform);
             float currentRowYPos = currentRow.transform.position.y;
-            float currentRowXScale = currentRow.transform.localScale.x;
             for (int j = 0; j < 5; j++)
             {
-                Instantiate(tileRef, new Vector3(boardRefX + (j * 1.2f * currentRowXScale), currentRowYPos, 9.5f), Quaternion.identity, currentRow.transform);
+                Instantiate(tile, new Vector3(boardPosX + (j * 1.2f * rowScaleX), currentRowYPos, 9.5f), Quaternion.identity, currentRow.transform);
             }
         }
+
+        // Spawn Player in center tile of 7x5 Board
+        Instantiate(player, new Vector3(boardPosX + (2 * 1.2f * rowScaleX), boardPosY + (3 * -0.95f), 9f), Quaternion.Euler(new Vector3(90, 0, 0)), board.transform);
     }
 }
