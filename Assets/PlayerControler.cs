87442c6543b23d09d3f27adc0efa004cbf49465e
MATsogas <mrst5gs@yahoo.gr>
2020-10-05 17:53:09 +0300

    Add player core whose color depends on tile bellow

---
+++ Assets/PlayerControler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    private GameObject playerCore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCore = gameObject.transform.Find("Player Core").gameObject; // Find player's core (child GameObject named PlayerCore)
+        
+        RaycastHit hit;
+        if (Physics.Raycast(gameObject.transform.position, transform.up, out hit))
+            playerCore.GetComponent<Renderer>().material.color = hit.collider.gameObject.GetComponent<Renderer>().material.GetColor("_Color");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/RandomColor.cs
+++ Assets/RandomColor.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class RandomColor : MonoBehaviour
 {
-    public GameObject cube;
-
     // Start is called before the first frame update
     void Start()
     {
-        cube.GetComponent<Renderer>().material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        gameObject.GetComponent<Renderer>().material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
     }
 }
