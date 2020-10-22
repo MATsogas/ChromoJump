bd9cb655f9d52eac018d94f036e36a8a8eb6f08f
MATsogas <mrst5gs@yahoo.gr>
2020-10-23 02:55:13 +0300

    Add basic main menu functionality

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Start Game
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    // Close Application
+    public void ExitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false; // Stops app from running in the editor
+        Application.Quit();
+    }
+}
