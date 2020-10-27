23bc00d437fe1f0c2b41f5ce6405e620d4e1ddfd
MATsogas <mrst5gs@yahoo.gr>
2020-10-27 10:28:16 +0200

    Improve Game Over screen buttons
    Have them be disabled for some seconds on Start

---
+++ Assets/WaitToBeEnabled.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaitToBeEnabled : MonoBehaviour
+{
+    public float timeDisabledOnStartInSeconds = 1f;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(timeDisabledOnStartInSeconds);
+        gameObject.GetComponent<Button>().interactable = true;
+    }
+}
