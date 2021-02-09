37fe501c822a25ddf392a57f1cfe7b7f204bfb4e
MATsogas <mrst5gs@yahoo.gr>
2021-02-09 07:11:34 +0200

    Add BG Music

---
+++ Assets/Scripts/KeepBetweenScenes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepBetweenScenes : MonoBehaviour
+{
+    static KeepBetweenScenes instance = null;
+
+    // Awake is called when the script instance is being loaded
+    void Awake()
+    {
+        // If an instance of this object exists, don't create a second one
+        if (instance!= null)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
